ea0f695286ea644984301e03febeb40bfc2e814a
Kijoo Pyo <72883209+kijoojoobi@users.noreply.github.com>
2020-12-18 12:36:56 -0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-console.log ("Hi! Welcome to my portfolio site")
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+console.log('Hi! Welcome to my portfolio site')
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
